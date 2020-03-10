affb0af80166a75cc4521f590ec726bf20002240
David Mathes <mathesd84@gmail.com>
2020-03-09 21:28:46 -0700

    Instantiated griddle to handle client data

--- src/containers/Clients/Clients.js
+++ src/containers/Clients/Clients.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react';
 import ClientMenu from '../../components/ClientMenu/ClientMenu';
+import Griddle, { plugins } from 'griddle-react';
+import styles from './Clients.module.css';
 
 class Clients extends Component {
+
     render() {
+
+        const data = [{
+            id: '1',
+            name: 'April',
+            address: '1234 5th St',
+            city: 'Raleigh',   
+            state: 'NC',
+            phone: '123-456-7890',
+            horses: [{name: 'Herman', color: 'white'}, {name: 'Moonbro', color: 'black'}]
+        },
+        {
+            id: '2',
+            name: 'Robert',
+            address: '125 8th St',
+            city: 'Durham',   
+            state: 'NC',
+            phone: '123-456-7887',
+            horses: [{name: 'Nigel', color: 'brown'}, {name: 'Mike', color: 'orange'}]
+        }];
+
         return(
             <div>
                 <ClientMenu />
-                <h1>Welcome to the Clients Page!</h1>
+                <div className={styles.GridContainer}>
+                    <Griddle 
+                        data={data}
+                        plugins={[plugins.LocalPlugin]}
+                     />
+                </div>
             </div>
         );
     }
